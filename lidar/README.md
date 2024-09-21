cd13c779474d6af4cd0edc4648c2a04f94f00ce1
gotodd <91164682+gotodd@users.noreply.github.com>
2024-09-21 14:12:19 -0700

    Add files via upload

---
+++ lidar/README.md
@@ -0,0 +1,1 @@
+Install the sweep-sdk python package from [https://github.com/scanse/sweep-sdk]
---
+++ lidar/test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import itertools
+from sweeppy import Sweep
+
+with Sweep('/dev/ttyUSB0') as sweep:
+    speed = sweep.get_motor_speed()
+    rate = sweep.get_sample_rate()
+
+    print('Motor Speed: {} Hz'.format(speed))
+    print('Sample Rate: {} Hz'.format(rate))
+
+    sweep.set_motor_speed(5)
+    sweep.set_sample_rate(500)
+
+    # Starts scanning as soon as the motor is ready
+    sweep.start_scanning()
+
+    # get_scans is coroutine-based generator lazily returning scans ad infinitum
+    for scan in itertools.islice(sweep.get_scans(), 300):
+        #print(scan)
+        for s in scan.samples:
+            if (s.distance<100 and s.signal_strength>100):
+                print(s)
+
+
